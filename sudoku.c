1536f3ffcfeb7d5407d29ddc9bac5a593f0d228a
Your Name <you@example.com>
2024-05-16 01:27:11 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -52,7 +52,6 @@ int is_valid(Node* n){
 List* get_adj_nodes(Node* n){
   List* list=createList(); //LISTA DE ADJUNTOS
   int i,j,k; //INDEXADORES
-  int aux; //AUXILIAR PARA EL INTERCAMBIO DE VALORES
   for(i=0;i<9;i++) { 
     for(j=0;j<9;j++) {
       if(n->sudo[i][j]==0) { //SI EL VALOR ES 0
@@ -68,7 +67,7 @@ List* get_adj_nodes(Node* n){
     }
   }
 
-  
+  }
   return list;
 }
 
